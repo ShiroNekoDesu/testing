8a424d3f613bb338bc948ca3609e0f7ad63f613b
simpi-pro <simpi.tfs@gmail.com>
2018-05-15 12:03:40 +0700

    add module

---
+++ Module/ModMasterClient.vb
@@ -0,0 +1,234 @@
+﻿Imports simpi.ParameterClient
+Imports simpi.MasterClient
+
+Module ModMasterClient
+    Friend dtClientMaster As New DataTable
+    Friend dtClientIndividu As New DataTable
+    Friend dtClientInstitusi As New DataTable
+    Friend dtClientDistributor As New DataTable
+    Friend dtClientIndividuSub As New DataTable
+    Friend dtClientInstitusiSub As New DataTable
+    Friend dtClientKYC As New DataTable
+
+    Friend dtParameterClientStatus As New DataTable
+    Friend dtParameterClientType As New DataTable
+    Friend dtParameterClientRisk As New DataTable
+    Friend dtParameterClientReligion As New DataTable
+    Friend dtParameterClientEducationLevel As New DataTable
+    Friend dtParameterClientOccupation As New DataTable
+    Friend dtParameterClientMarital As New DataTable
+    Friend dtParameterClientDocumentType As New DataTable
+    Friend dtParameterClientBusinessType As New DataTable
+    Friend dtParameterClientBusinessActivity As New DataTable
+
+    Friend Function GetClientMaster() As DataTable
+        If dtClientMaster IsNot Nothing AndAlso dtClientMaster.Rows.Count > 0 Then Return dtClientMaster Else Return GetClientMasterUpdate()
+    End Function
+
+    Friend Function GetClientMasterUpdate() As DataTable
+        Dim objClient As New MasterClient
+        objClient.UserAccess = objAccess
+        dtClientMaster = objClient.Search(objMasterSimpi, "")
+        dtClientMaster.CaseSensitive = False
+        Return dtClientMaster
+    End Function
+
+    Friend Function GetClientIndividu() As DataTable
+        If dtClientIndividu IsNot Nothing AndAlso dtClientIndividu.Rows.Count > 0 Then Return dtClientIndividu Else Return GetClientIndividuUpdate()
+    End Function
+
+    Friend Function GetClientIndividuUpdate() As DataTable
+        Dim objClient As New MasterClientIndividu
+        objClient.UserAccess = objAccess
+        dtClientIndividu = objClient.Search(objMasterSimpi, "")
+        dtClientIndividu.CaseSensitive = False
+        Return dtClientIndividu
+    End Function
+
+    Friend Function GetClientInstitusi() As DataTable
+        If dtClientInstitusi IsNot Nothing AndAlso dtClientInstitusi.Rows.Count > 0 Then Return dtClientInstitusi Else Return GetClientInstitusiUpdate()
+    End Function
+
+    Friend Function GetClientInstitusiUpdate() As DataTable
+        Dim objClient As New MasterClientInstitution
+        objClient.UserAccess = objAccess
+        dtClientInstitusi = objClient.Search(objMasterSimpi, "")
+        dtClientInstitusi.CaseSensitive = False
+        Return dtClientInstitusi
+    End Function
+
+    Friend Function GetClientIndividuSub() As DataTable
+        If dtClientIndividuSub IsNot Nothing AndAlso dtClientIndividuSub.Rows.Count > 0 Then Return dtClientIndividuSub Else Return GetClientIndividuSubUpdate()
+    End Function
+
+    Friend Function GetClientIndividuSubUpdate() As DataTable
+        Dim objClient As New MasterClientIndividuSub
+        objClient.UserAccess = objAccess
+        dtClientIndividuSub = objClient.Search(objMasterSimpi, "")
+        dtClientIndividuSub.CaseSensitive = False
+        Return dtClientIndividuSub
+    End Function
+
+    Friend Function GetClientInstitusiSub() As DataTable
+        If dtClientInstitusiSub IsNot Nothing AndAlso dtClientInstitusiSub.Rows.Count > 0 Then Return dtClientInstitusiSub Else Return GetClientInstitusiSubUpdate()
+    End Function
+
+    Friend Function GetClientInstitusiSubUpdate() As DataTable
+        Dim objClient As New MasterClientInstitutionSub
+        objClient.UserAccess = objAccess
+        dtClientInstitusiSub = objClient.Search(objMasterSimpi, "")
+        dtClientInstitusiSub.CaseSensitive = False
+        Return dtClientInstitusiSub
+    End Function
+
+    Friend Function GetClientDistributor() As DataTable
+        If dtClientDistributor IsNot Nothing AndAlso dtClientDistributor.Rows.Count > 0 Then Return dtClientDistributor Else Return GetClientDistributorUpdate()
+    End Function
+
+    Friend Function GetClientDistributorUpdate() As DataTable
+        Dim objClient As New MasterClientDistributor
+        objClient.UserAccess = objAccess
+        dtClientDistributor = objClient.Search(objMasterSimpi, "")
+        dtClientDistributor.CaseSensitive = False
+        Return dtClientDistributor
+    End Function
+
+    Friend Function GetClientKYC() As DataTable
+        If dtClientKYC IsNot Nothing AndAlso dtClientKYC.Rows.Count > 0 Then Return dtClientKYC Else Return GetClientKYCUpdate()
+    End Function
+
+    Friend Function GetClientKYCUpdate() As DataTable
+        Dim objClient As New ClientKYC
+        objClient.UserAccess = objAccess
+        dtClientKYC = objClient.Search(objMasterSimpi)
+        dtClientKYC.CaseSensitive = False
+        Return dtClientKYC
+    End Function
+
+    Friend Sub GetComboGender(ByVal cmb As C1.Win.C1InputPanel.InputComboBox)
+        cmb.Items.Clear()
+        cmb.Items.AddText("M")
+        cmb.Items.AddText("F")
+    End Sub
+
+    Friend Function GetParameterClientStatus() As DataTable
+        If dtParameterClientStatus IsNot Nothing AndAlso dtParameterClientStatus.Rows.Count > 0 Then Return dtParameterClientStatus Else Return GetParameterClientStatusUpdate()
+    End Function
+
+    Friend Function GetParameterClientStatusUpdate() As DataTable
+        Dim objStatus As New ParameterClientStatus
+        objStatus.UserAccess = objAccess
+        dtParameterClientStatus = objStatus.Search("")
+        dtParameterClientStatus.CaseSensitive = False
+        Return dtParameterClientStatus
+    End Function
+
+    Friend Function GetParameterClientType() As DataTable
+        If dtParameterClientType IsNot Nothing AndAlso dtParameterClientType.Rows.Count > 0 Then Return dtParameterClientType Else Return GetParameterClientTypeUpdate()
+    End Function
+
+    Friend Function GetParameterClientTypeUpdate() As DataTable
+        Dim objAsset As New ParameterClientType
+        objAsset.UserAccess = objAccess
+        dtParameterClientType = objAsset.Search("")
+        dtParameterClientType.CaseSensitive = False
+        Return dtParameterClientType
+    End Function
+
+    Friend Function GetParameterClientRisk() As DataTable
+        If dtParameterClientRisk IsNot Nothing AndAlso dtParameterClientRisk.Rows.Count > 0 Then Return dtParameterClientRisk Else Return GetParameterClientRiskUpdate()
+    End Function
+
+    Friend Function GetParameterClientRiskUpdate() As DataTable
+        Dim objRisk As New ParameterClientRisk
+        objRisk.UserAccess = objAccess
+        dtParameterClientRisk = objRisk.Search("")
+        dtParameterClientRisk.CaseSensitive = False
+        Return dtParameterClientRisk
+    End Function
+
+    Friend Function GetParameterClientReligion() As DataTable
+        If dtParameterClientReligion IsNot Nothing AndAlso dtParameterClientReligion.Rows.Count > 0 Then Return dtParameterClientReligion Else Return GetParameterClientReligionUpdate()
+    End Function
+
+    Friend Function GetParameterClientReligionUpdate() As DataTable
+        Dim objReligion As New ParameterReligion
+        objReligion.UserAccess = objAccess
+        dtParameterClientReligion = objReligion.Search("")
+        dtParameterClientReligion.CaseSensitive = False
+        Return dtParameterClientReligion
+    End Function
+
+    Friend Function GetParameterClientEducationLevel() As DataTable
+        If dtParameterClientEducationLevel IsNot Nothing AndAlso dtParameterClientEducationLevel.Rows.Count > 0 Then Return dtParameterClientEducationLevel Else Return GetParameterClientEducationLevelUpdate()
+    End Function
+
+    Friend Function GetParameterClientEducationLevelUpdate() As DataTable
+        Dim objType As New ParameterEducationLevel
+        objType.UserAccess = objAccess
+        dtParameterClientEducationLevel = objType.Search("")
+        dtParameterClientEducationLevel.CaseSensitive = False
+        Return dtParameterClientEducationLevel
+    End Function
+
+    Friend Function GetParameterClientOccupation() As DataTable
+        If dtParameterClientOccupation IsNot Nothing AndAlso dtParameterClientOccupation.Rows.Count > 0 Then Return dtParameterClientOccupation Else Return GetParameterClientOccupationUpdate()
+    End Function
+
+    Friend Function GetParameterClientOccupationUpdate() As DataTable
+        Dim objType As New ParameterOccupation
+        objType.UserAccess = objAccess
+        dtParameterClientOccupation = objType.Search("")
+        dtParameterClientOccupation.CaseSensitive = False
+        Return dtParameterClientOccupation
+    End Function
+
+    Friend Function GetParameterClientMarital() As DataTable
+        If dtParameterClientMarital IsNot Nothing AndAlso dtParameterClientMarital.Rows.Count > 0 Then Return dtParameterClientMarital Else Return GetParameterClientMaritalUpdate()
+    End Function
+
+    Friend Function GetParameterClientMaritalUpdate() As DataTable
+        Dim objType As New ParameterMaritalStatus
+        objType.UserAccess = objAccess
+        dtParameterClientMarital = objType.Search("")
+        dtParameterClientMarital.CaseSensitive = False
+        Return dtParameterClientMarital
+    End Function
+
+    Friend Function GetParameterClientDocumentType() As DataTable
+        If dtParameterClientDocumentType IsNot Nothing AndAlso dtParameterClientDocumentType.Rows.Count > 0 Then Return dtParameterClientDocumentType Else Return GetParameterClientDocumentTypeUpdate()
+    End Function
+
+    Friend Function GetParameterClientDocumentTypeUpdate() As DataTable
+        Dim objType As New ParameterDocumentType
+        objType.UserAccess = objAccess
+        dtParameterClientDocumentType = objType.Search("")
+        dtParameterClientDocumentType.CaseSensitive = False
+        Return dtParameterClientDocumentType
+    End Function
+
+    Friend Function GetParameterClientBusinessType() As DataTable
+        If dtParameterClientBusinessType IsNot Nothing AndAlso dtParameterClientBusinessType.Rows.Count > 0 Then Return dtParameterClientBusinessType Else Return GetParameterClientBusinessTypeUpdate()
+    End Function
+
+    Friend Function GetParameterClientBusinessTypeUpdate() As DataTable
+        Dim objType As New ParameterBusinessType
+        objType.UserAccess = objAccess
+        dtParameterClientBusinessType = objType.Search("")
+        dtParameterClientBusinessType.CaseSensitive = False
+        Return dtParameterClientBusinessType
+    End Function
+
+    Friend Function GetParameterClientBusinessActivity() As DataTable
+        If dtParameterClientBusinessActivity IsNot Nothing AndAlso dtParameterClientBusinessActivity.Rows.Count > 0 Then Return dtParameterClientBusinessActivity Else Return GetParameterClientBusinessActivityUpdate()
+    End Function
+
+    Friend Function GetParameterClientBusinessActivityUpdate() As DataTable
+        Dim objType As New ParameterBusinessActivity
+        objType.UserAccess = objAccess
+        dtParameterClientBusinessActivity = objType.Search("")
+        dtParameterClientBusinessActivity.CaseSensitive = False
+        Return dtParameterClientBusinessActivity
+    End Function
+
+End Module
---
+++ Module/ModParameter.vb
@@ -0,0 +1,163 @@
+﻿Imports simpi.MasterSecurities
+Imports simpi.ParameterTA
+Imports simpi.MarketCompany
+Imports simpi.ParameterSecurities
+
+Module ModParameter
+    Friend dtParameterCountry As New DataTable
+    Friend dtParameterCity As New DataTable
+    Friend dtParameterBenchmarkClass As New DataTable
+    Friend dtParameterSectorClass As New DataTable
+    Friend dtParameterInstrumentType As New DataTable
+    Friend dtParameterInstrumentSubType As New DataTable
+    Friend dtParameterTATrxType1 As New DataTable
+    Friend dtParameterBroker As New DataTable
+    Friend dtParameterFACharges As New DataTable
+    Friend dtParameterExternalSystem As New DataTable
+    Friend dtParameterTADistributionType As New DataTable
+    Friend dtParameterTADistributionStatus As New DataTable
+
+    Friend Function GetParameterCountry() As DataTable
+        If dtParameterCountry IsNot Nothing AndAlso dtParameterCountry.Rows.Count > 0 Then Return dtParameterCountry Else Return GetParameterCountryUpdate()
+    End Function
+
+    Friend Function GetParameterCountryUpdate() As DataTable
+        Dim objCcy As New ParameterCountry
+        objCcy.UserAccess = objAccess
+        dtParameterCountry = objCcy.Search("")
+        dtParameterCountry.CaseSensitive = False
+        Return dtParameterCountry
+    End Function
+
+    Friend Function GetParameterCity() As DataTable
+        If dtParameterCity IsNot Nothing AndAlso dtParameterCity.Rows.Count > 0 Then Return dtParameterCity Else Return GetParameterCityUpdate()
+    End Function
+
+    Friend Function GetParameterCityUpdate() As DataTable
+        Dim objCity As New ParameterCity
+        objCity.UserAccess = objAccess
+        dtParameterCity = objCity.Search(0, 0, "")
+        dtParameterCity.CaseSensitive = False
+        Return dtParameterCity
+    End Function
+
+    Friend Function GetParameterBenchmarkClass() As DataTable
+        If dtParameterBenchmarkClass IsNot Nothing AndAlso dtParameterBenchmarkClass.Rows.Count > 0 Then Return dtParameterBenchmarkClass Else Return GetParameterBenchmarkClassUpdate()
+    End Function
+
+    Friend Function GetParameterBenchmarkClassUpdate() As DataTable
+        Dim objClass As New ParameterBenchmarkClass
+        objClass.UserAccess = objAccess
+        dtParameterBenchmarkClass = objClass.Search("")
+        dtParameterBenchmarkClass.CaseSensitive = False
+        Return dtParameterBenchmarkClass
+    End Function
+
+    Friend Function GetParameterSectorClass() As DataTable
+        If dtParameterSectorClass IsNot Nothing AndAlso dtParameterSectorClass.Rows.Count > 0 Then Return dtParameterSectorClass Else Return GetParameterSectorClassUpdate()
+    End Function
+
+    Friend Function GetParameterSectorClassUpdate() As DataTable
+        Dim objClass As New ParameterSectorClass
+        objClass.UserAccess = objAccess
+        dtParameterSectorClass = objClass.Search("")
+        dtParameterSectorClass.CaseSensitive = False
+        Return dtParameterSectorClass
+    End Function
+
+    Friend Function GetParameterInstrumentType() As DataTable
+        If dtParameterInstrumentType IsNot Nothing AndAlso dtParameterInstrumentType.Rows.Count > 0 Then Return dtParameterInstrumentType Else Return GetParameterInstrumentTypeUpdate()
+    End Function
+
+    Friend Function GetParameterInstrumentTypeUpdate() As DataTable
+        Dim objType As New ParameterInstrumentType
+        objType.UserAccess = objAccess
+        dtParameterInstrumentType = objType.Search("")
+        dtParameterInstrumentType.CaseSensitive = False
+        Return dtParameterInstrumentType
+    End Function
+
+    Friend Function GetParameterInstrumentSubType() As DataTable
+        If dtParameterInstrumentSubType IsNot Nothing AndAlso dtParameterInstrumentSubType.Rows.Count > 0 Then Return dtParameterInstrumentSubType Else Return GetParameterInstrumentSubTypeUpdate()
+    End Function
+
+    Friend Function GetParameterInstrumentSubTypeUpdate() As DataTable
+        Dim objSub As New ParameterInstrumentSubType
+        objSub.UserAccess = objAccess
+        dtParameterInstrumentSubType = objSub.Search("")
+        dtParameterInstrumentSubType.CaseSensitive = False
+        Return dtParameterInstrumentSubType
+    End Function
+
+    Friend Function GetParameterExternalSystem() As DataTable
+        If dtParameterExternalSystem IsNot Nothing AndAlso dtParameterExternalSystem.Rows.Count > 0 Then Return dtParameterExternalSystem Else Return GetParameterExternalSystemUpdate()
+    End Function
+
+    Friend Function GetParameterExternalSystemUpdate() As DataTable
+        Dim objSystem As New ParameterExternalSystem
+        objSystem.UserAccess = objAccess
+        dtParameterExternalSystem = objSystem.Search("")
+        dtParameterExternalSystem.CaseSensitive = False
+        Return dtParameterExternalSystem
+    End Function
+
+    Friend Function GetParameterFACharges() As DataTable
+        If dtParameterFACharges IsNot Nothing AndAlso dtParameterFACharges.Rows.Count > 0 Then Return dtParameterFACharges Else Return GetParameterFAChargesUpdate()
+    End Function
+
+    Friend Function GetParameterFAChargesUpdate() As DataTable
+        Dim objFee As New simpi.ParameterFA.ParameterCharges
+        objFee.UserAccess = objAccess
+        dtParameterFACharges = objFee.Search("")
+        dtParameterFACharges.CaseSensitive = False
+        Return dtParameterFACharges
+    End Function
+
+    Friend Function GetParameterBroker(ByVal CountryID As Integer, ByVal strKeyword As String) As DataTable
+        If dtParameterBroker IsNot Nothing AndAlso dtParameterBroker.Rows.Count > 0 Then Return dtParameterBroker Else Return GetParameterBrokerUpdate()
+    End Function
+
+    Friend Function GetParameterBrokerUpdate() As DataTable
+        Dim objBroker As New MasterCounterPart
+        objBroker.UserAccess = objAccess
+        dtParameterBroker = objBroker.Search(0, "")
+        dtParameterBroker.CaseSensitive = False
+        Return dtParameterBroker
+    End Function
+
+    Friend Function GetParameterTATrxType1() As DataTable
+        If dtParameterTATrxType1 IsNot Nothing AndAlso dtParameterTATrxType1.Rows.Count > 0 Then Return dtParameterTATrxType1 Else Return GetParameterTATrxType1Update()
+    End Function
+
+    Friend Function GetParameterTATrxType1Update() As DataTable
+        Dim objType As New ParameterTransactionTrxType1
+        objType.UserAccess = objAccess
+        dtParameterTATrxType1 = objType.Search("")
+        dtParameterTATrxType1.CaseSensitive = False
+        Return dtParameterTATrxType1
+    End Function
+
+    Friend Function GetParameterTADistributionType() As DataTable
+        If dtParameterTADistributionType IsNot Nothing AndAlso dtParameterTADistributionType.Rows.Count > 0 Then Return dtParameterTADistributionType Else Return GetParameterTADistributionTypeUpdate()
+    End Function
+
+    Friend Function GetParameterTADistributionTypeUpdate() As DataTable
+        Dim objType As New ParameterDistributionType
+        objType.UserAccess = objAccess
+        dtParameterTADistributionType = objType.Search("")
+        dtParameterTADistributionType.CaseSensitive = False
+        Return dtParameterTADistributionType
+    End Function
+
+    Friend Function GetParameterTADistributionStatus() As DataTable
+        If dtParameterTADistributionStatus IsNot Nothing AndAlso dtParameterTADistributionStatus.Rows.Count > 0 Then Return dtParameterTADistributionStatus Else Return GetParameterTADistributionStatusUpdate()
+    End Function
+
+    Friend Function GetParameterTADistributionStatusUpdate() As DataTable
+        Dim objStatus As New ParameterDistributionStatus
+        objStatus.UserAccess = objAccess
+        dtParameterTADistributionStatus = objStatus.Search("")
+        dtParameterTADistributionStatus.CaseSensitive = False
+        Return dtParameterTADistributionStatus
+    End Function
+End Module
